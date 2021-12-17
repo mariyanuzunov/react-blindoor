66587ac1d9f3af24a18902441e8fd58ce529fb6f
Mariyan Uzunov <themariyanuzunov@gmail.com>
2021-12-17 18:14:34 +0200

    contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,3 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import {
+    faEnvelopeSquare,
+    faPhoneSquare,
+} from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Contacts.module.css';
+
 export default function Contacts() {
-    return <h1>Contacts Page</h1>;
+    return (
+        <section className={styles['contacts-container']}>
+            <h4>Блиндор ЕООД</h4>
+            <p>Може да се свържете с нас по следните начини:</p>
+            <ul>
+                <li>
+                    <FontAwesomeIcon icon={faPhoneSquare} />
+                    <span>+359 881 122 121</span>
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faEnvelopeSquare}
+                        className={styles['icon']}
+                    />
+                    <span>blindoorltd@test.com</span>
+                </li>
+                <li>
+                    <FontAwesomeIcon
+                        icon={faFacebookSquare}
+                        className={styles['icon']}
+                    />
+                    <span>www.fb.com/blindoorltd</span>
+                </li>
+            </ul>
+            <p>
+                или да посетите нашия шоурум на адрес
+                <strong> бул. Княз Александър Дондуков 1</strong>
+            </p>
+        </section>
+    );
 }
